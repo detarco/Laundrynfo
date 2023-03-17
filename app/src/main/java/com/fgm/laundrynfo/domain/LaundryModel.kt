6c36f2dca699fcdacd932e79d0a12a34a8fef997
Fernando González Maroto <fernandinator@gmail.com>
2023-03-17 01:40:49 +0100

    Last retouches

--- app/src/main/java/com/fgm/laundrynfo/domain/LaundryModel.kt
+++ app/src/main/java/com/fgm/laundrynfo/domain/LaundryModel.kt
@@ -1,14 +1,5 @@
 package com.fgm.laundrynfo.domain
 
-data class ClientModel(
-    val id: Int,
-    val name: String,
-    val surname: String,
-    val phone: Int,
-    val email: String,
-    val address: String
-)
-
 data class CustomerModel(
     val id: Int,
     val name: String,
